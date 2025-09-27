6cb07b64ce5e6a9dcc6756c5950d3c8e54dc950c
Shrssss <haurnnia764@gmail.com>
2025-09-27 23:56:48 +0900

    SquareAPI導入に向けたファイル等の追加
    add:APIController,PaymentService,SquareConfig.javaの追加
    add:application.yaml、API接続に関する記述の追加

---
+++ demo/src/main/java/com/example/demo/config/SquareConfig.java
@@ -0,0 +1,9 @@
+package com.example.demo.config;
+
+public class SquareConfig {
+
+	public SquareConfig() {
+		
+	}
+
+}
---
+++ demo/src/main/java/com/example/demo/controller/APIController.java
@@ -0,0 +1,9 @@
+package com.example.demo.controller;
+
+public class APIController {
+
+	public APIController() {
+		
+	}
+
+}
