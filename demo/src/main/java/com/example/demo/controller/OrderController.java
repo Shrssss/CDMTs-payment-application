91068b2c74eab57bdc54c8cbf07d82101281057d
Shrssss <haurnnia764@gmail.com>
2025-09-30 16:26:04 +0900

    Refactor:コメントアウトを見やすく修正

--- demo/src/main/java/com/example/demo/controller/OrderController.java
+++ demo/src/main/java/com/example/demo/controller/OrderController.java
@@ -17,32 +17,32 @@ public class OrderController {
 	
 	private final OrderService service;
     
-    @PostMapping("/order/set") //order.jsonをDB登録
+    @PostMapping("/order/set") //フロントから受け取ったorder.jsonをDB登録
     public OrderTable createOrder(@RequestBody OrderRequest request) {
     	return service.createOrder(request);
     }
     
-    @GetMapping("/order/get/{orderId}") //order.jsonを送信
+    @GetMapping("/order/get/{orderId}") //order.jsonをフロントに送信
     public Order getOrder(@PathVariable int orderId) {
     	return service.getOrder(orderId);
     }
     
-    @PostMapping("/order/set/status/{orderId}/{tf}") //servingStatusを更新
+    @PostMapping("/order/set/status/{orderId}/{tf}") //orderId,tfをもとにservingStatusを更新 （処理内容 (tf)? statusを次の状態へ : statusを前の状態へ）
     public OrderTable changeServingStatus(int orderId,boolean tf) {
     	return service.changeServingStatus(orderId,tf);
     }
     
-    @GetMapping("/order/get/status/{orderId}") //servingStatusを送信
+    @GetMapping("/order/get/status/{orderId}") //servingStatusをフロントに送信
     public String selectServingStatusByOrderId(@PathVariable int orderId) {
     	return service.selectServingStatusByOrderId(orderId);
     }
     
-    @GetMapping("/items/get/{itemId}") //Item.jsonを送信
+    @GetMapping("/items/get/{itemId}") //Item.jsonをフロントに送信
     public Item selectItemByItemId(@PathVariable int itemId) {
     	return service.selectItemByItemId(itemId);
     }
 
-	@PostMapping("/items/set/available/{itemId}/{available}") //ItemAvailをupdate
+	@PostMapping("/items/set/available/{itemId}/{available}") //itemId,availableをもとにItemAvailを更新
 	public Item toggleAvailablity(@PathVariable int itemId,@PathVariable boolean available) {
 		return service.toggleAvailablity(itemId,available);
 	}
