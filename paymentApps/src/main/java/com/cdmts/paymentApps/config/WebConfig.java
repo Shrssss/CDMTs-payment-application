7b0c3de192a6796de32ab93eafb1c03ff9267404
Shrssss <haurnnia764@gmail.com>
2025-10-14 21:53:38 +0900

    fix:WebConfig,apiがはじかれないように修正

--- paymentApps/src/main/java/com/cdmts/paymentApps/config/WebConfig.java
+++ paymentApps/src/main/java/com/cdmts/paymentApps/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/{path:[^\\.]*}")
+		registry.addViewController("/{path:^(?!api).*$}")
 		.setViewName("forward:/index.html");
 	}
 }
