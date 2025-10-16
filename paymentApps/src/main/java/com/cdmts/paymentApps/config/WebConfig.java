6a16ce5666ed2d4da50e5f0dda7f2a4ccba54dbb
Melting Sugar <83892914+Kota-Ichikawa@users.noreply.github.com>
2025-10-16 17:10:04 +0900

    modify uncorrect code

--- paymentApps/src/main/java/com/cdmts/paymentApps/config/WebConfig.java
+++ paymentApps/src/main/java/com/cdmts/paymentApps/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.cdmts.paymentApps.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,11 +14,12 @@ public class WebConfig implements WebMvcConfigurer {
 		
 		registry.addViewController("/")
 		.setViewName("forward:/index.html");
-
+	}
+	
 		@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("https://dev-cdmts-pay.codemates.net"/) // ← HTTPSを明示
+            .allowedOrigins("https://dev-cdmts-pay.codemates.net") // ← HTTPSを明示
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowCredentials(true);
 	}
