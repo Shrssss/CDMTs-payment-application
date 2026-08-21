3b971b5340f8ff980a6deae9745e4e440b668de1
Shrssss <harunnia764@gmail.com>
2026-08-21 20:50:08 +0900

    refactor:itemに関するコードの再配置

--- paymentApps/src/main/java/com/cdmts/paymentApps/controller/ItemController.java
+++ paymentApps/src/main/java/com/cdmts/paymentApps/controller/ItemController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cdmts.paymentApps.model.entity.Item;
@@ -22,18 +24,48 @@ public class ItemController {
 	
 	private final ItemService itemService;
 	
-    @GetMapping("/get/byitemId/{itemId}") //Item.jsonをフロントに送信
-    public Item selectItemByItemId(@PathVariable int itemId) {
+    /*
+     * 商品検索
+     * 
+     * メソッド名 	: createOrder
+     * 戻り値		: Long orderId
+     * 引数		: OrderCreateRequest
+     * 
+     * 		GET /api/items
+     * 
+     */
+    @GetMapping("/get/byItemIds")
+    public List<Item> getItemsByItemIds(@RequestParam List<Long> itemIds) {
     	return itemService.selectItemByItemId(itemId);
     }
     
+    /*
+     * 全商品取得
+     * 
+     * メソッド名 	: selectAllItems
+     * 戻り値		: List<Item>
+     * 引数		: 
+     * 
+     * 		GET /api/items/get/allItems
+     * 
+     */
     @GetMapping("/get/allItems")
     public List<Item> selectAllItems(){
     	return itemService.selectAllItems();
     }
 
-	@PostMapping("/set/available/{itemId}/{available}") //itemId,availableをもとにItemAvailを更新
-	public List<Item> toggleAvailablity(@PathVariable int itemId,@PathVariable boolean available) {
+    /*
+     * 在庫状況更新
+     * 
+     * メソッド名 	: updateAvailablity
+     * 戻り値		: List<Item>
+     * 引数		: List<Long> itemId, Boolean available
+     * 
+     * 		PUT /api/items/update/available/{available}
+     * 
+     */
+	@PutMapping("/update/available/{available}")
+	public List<Item> updateAvailablity(@RequestParam List<Long> itemIds,@PathVariable Boolean available) {
 		return itemService.toggleAvailablity(itemId,available);
 	}
 
--- paymentApps/src/main/java/com/cdmts/paymentApps/service/ItemService.java
+++ paymentApps/src/main/java/com/cdmts/paymentApps/service/ItemService.java
@@ -1,9 +1,72 @@
 package com.cdmts.paymentApps.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Service;
+
+import com.cdmts.paymentApps.mapper.ItemMapper;
+import com.cdmts.paymentApps.model.entity.Item;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ItemService {
+	
+	private final ItemMapper itemMapper;
 
-	public ItemService() {
-		// TODO Auto-generated constructor stub
+	public Item selectItemByItemId(@Param("itemId") int itemId){
+		return mapper.selectItemByItemId(itemId);
 	}
+	
+    public List<Item> selectAllItems(){
+    	return mapper.selectAllItems();
+    }
+    
+    /** Itemの在庫情報を更新し、Itemを返す　*/
+    public List<Item> toggleAvailablity(int itemId,boolean available) {
+    	
+    	List<Item> items=new ArrayList<>();
+    	
+    	if(itemId%10==0) {
+    		
+    		int[] itemIds= {itemId,itemId+31,itemId+32,itemId+33,itemId+34};
+    		int updated=0;
+    				
+    		for(int i=0;i<5;i++) {
+    			
+    			updated+=updateItemAvailabilityByItemId(itemIds[i],available);
+    			
+    			if(updated==0) {
+           		 throw new IllegalArgumentException("指定されたitemIdが存在しません: "+itemIds[i]);
+    			}
+            	
+            	items.add(selectItemByItemId(itemId));
+    			
+    		}
 
+    	}else {
+    		
+    		int[] itemIds={itemId,90+itemId%10};
+    		
+    		int updated=0;
+    		
+    		for(int i=0;i<2;i++) {
+    			updated+=updateItemAvailabilityByItemId(itemIds[i],available);
+    			
+    			if(updated==0) {
+           		 throw new IllegalArgumentException("指定されたitemIdが存在しません: "+itemIds[i]);
+    			}
+    			
+    			items.add(selectItemByItemId(itemIds[i]));
+    			
+    		}
+    	}
+    	
+    	return items;
+    	
+    }
+    
 }
